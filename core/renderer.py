411ea7a88123b11e2c9bbd1027e278e6bef77a45
yuhshar <yashar.hoss@gmail.com>
2026-04-10 09:13:46 -0400

    Update renderer.py

--- core/renderer.py
+++ core/renderer.py
@@ -29,7 +29,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.colors import Color
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.lib.units import pt
+
 
 from core.layout_engine import ReconfigurationProposal, BubblePolygon
 
